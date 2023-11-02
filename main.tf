fd6f73dfa52c7711ea321b17e7f29ebd51588be4
lucas vieira <lucas.vss12@gmail.com>
2023-11-02 11:58:26 -0300

    Tentativa para adicionar permissão para invocar lambda

--- main.tf
+++ main.tf
@@ -46,31 +46,6 @@ resource "aws_api_gateway_deployment" "mikes-api-gateway-deployment" {
   stage_name  = "dev"
 }
 
-#output "api_gateway_invoke_url" {
-#  value = aws_api_gateway_deployment.mikes-api-gateway-deployment.invoke_url
-#}
-#
-#resource "aws_iam_policy" "api_gateway_invoke_lambda_policy" {
-#  name        = "APIGatewayInvokeLambdaPolicy"
-#  description = "Policy to allow API Gateway to invoke Lambda functions"
-#
-#  policy = jsonencode({
-#    Version = "2012-10-17",
-#    Statement = [
-#      {
-#        Action = "sts:AssumeRole",
-#        Effect = "Allow",
-#        Resource = "*"
-#      },
-#      {
-#        Effect = "Allow",
-#        Action = "lambda:InvokeFunction",
-#        Resource = "arn:aws:lambda:us-east-2:644237782704:function/mikes_lambda_authorizer"
-#      }
-#    ]
-#  })
-#}
-
 resource "aws_lambda_permission" "mikes_lambda_authorizer_permission" {
   statement_id  = "AllowAPIInvoke"
   action        = "lambda:InvokeFunction"
