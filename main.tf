ea9b0e237509a9bc74f90fb2fddb1dda91623853
wagnerlm <wagnerlink@gmail.com>
2023-11-04 10:50:47 -0300

    .

--- main.tf
+++ main.tf
@@ -16,45 +16,6 @@ resource "aws_api_gateway_resource" "auth_resource" {
   path_part   = "auth"
 }
 
-resource "aws_api_gateway_method" "auth_method" {
-  rest_api_id   = aws_api_gateway_rest_api.mikes_api_gateway.id
-  resource_id   = aws_api_gateway_resource.auth_resource.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.mikes_api_gateway.id
-  resource_id             = aws_api_gateway_resource.auth_resource.id
-  http_method             = aws_api_gateway_method.auth_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-2:644237782704:function:mikes_lambda_authorizer/invocations"
-}
-
-resource "aws_api_gateway_authorizer" "cognito_authorizer" {
-  name            = "cognito-authorizer"
-  rest_api_id     = aws_api_gateway_rest_api.mikes_api_gateway.id
-  type            = "COGNITO_USER_POOLS"
-  identity_source = "method.request.header.Authorization"
-  provider_arns   = ["arn:aws:cognito-idp:us-east-2:644237782704:userpool/us-east-2_2BLwlHbmP"]
-}
-
-resource "aws_api_gateway_deployment" "mikes-api-gateway-deployment" {
-  depends_on  = [aws_api_gateway_integration.lambda_integration]
-  rest_api_id = aws_api_gateway_rest_api.mikes_api_gateway.id
-  stage_name  = "dev"
-}
-
-resource "aws_lambda_permission" "mikes_lambda_authorizer_permission" {
-  statement_id  = "AllowAPIInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = data.aws_lambda_function.mikes_lambda_authorizer.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_rest_api.mikes_api_gateway.execution_arn}/*"
-}
-
 resource "aws_api_gateway_resource" "customer_resource" {
   rest_api_id = aws_api_gateway_rest_api.mikes_api_gateway.id
   parent_id   = aws_api_gateway_resource.auth_resource.id
@@ -67,19 +28,65 @@ resource "aws_api_gateway_resource" "variable_customer_resource" {
   path_part   = "{customer_id}"
 }
 
+resource "aws_api_gateway_method" "auth_method" {
+  rest_api_id   = aws_api_gateway_rest_api.mikes_api_gateway.id
+  resource_id   = aws_api_gateway_resource.auth_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "get_customer_method" {
+  rest_api_id   = aws_api_gateway_rest_api.mikes_api_gateway.id
+  resource_id   = aws_api_gateway_resource.customer_resource.id
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"  # Use a autorização do cognito_authorizer
+}
 
 resource "aws_api_gateway_method" "get_variable_customer_method" {
   rest_api_id   = aws_api_gateway_rest_api.mikes_api_gateway.id
   resource_id   = aws_api_gateway_resource.variable_customer_resource.id
   http_method   = "GET"
-  authorization = "COGNITO_USER_POOLS"
+  authorization = "COGNITO_USER_POOLS"  # Use a autorização do cognito_authorizer
+  authorizer_id = aws_api_gateway_authorizer.cognito_authorizer.id
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.mikes_api_gateway.id
+  resource_id             = aws_api_gateway_resource.auth_resource.id
+  http_method             = aws_api_gateway_method.auth_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-2:644237782704:function:mikes_lambda_authorizer/invocations"
 }
 
 resource "aws_api_gateway_integration" "get_customer_integration" {
   rest_api_id             = aws_api_gateway_rest_api.mikes_api_gateway.id
-  resource_id             = aws_api_gateway_resource.customer_resource.id
+  resource_id             = aws_api_gateway_resource.variable_customer_resource.id
   http_method             = aws_api_gateway_method.get_customer_method.http_method
   integration_http_method = "GET"
   type                    = "HTTP_PROXY"
   uri                     = "http://10.0.19.66:8080/customers/{customer_id}"
+}
+
+resource "aws_api_gateway_authorizer" "cognito_authorizer" {
+  name            = "cognito-authorizer"
+  rest_api_id     = aws_api_gateway_rest_api.mikes_api_gateway.id
+  type            = "COGNITO_USER_POOLS"
+  identity_source = "method.request.header.Authorization"
+  provider_arns   = ["arn:aws:cognito-idp:us-east-2:644237782704:userpool/us-east-2_2BLwlHbmP"]
+}
+
+resource "aws_api_gateway_deployment" "mikes-api-gateway-deployment" {
+  depends_on  = [aws_api_gateway_integration.lambda_integration, aws_api_gateway_integration.get_customer_integration]
+  rest_api_id = aws_api_gateway_rest_api.mikes_api_gateway.id
+  stage_name  = "dev"
+}
+
+resource "aws_lambda_permission" "mikes_lambda_authorizer_permission" {
+  statement_id  = "AllowAPIInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = data.aws_lambda_function.mikes_lambda_authorizer.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.mikes_api_gateway.execution_arn}/*"
 }
