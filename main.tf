82481213e6e6a2e83142a99163eeea0939d7ebe4
lucas vieira <lucas.vss12@gmail.com>
2023-11-04 15:42:07 -0300

    Ajusta rota get para api product

--- main.tf
+++ main.tf
@@ -47,6 +47,7 @@ resource "aws_api_gateway_resource" "order_resource" {
   path_part   = "orders"
 }
 
+
 resource "aws_api_gateway_resource" "variable_product_resource" {
   rest_api_id = aws_api_gateway_rest_api.mikes_api_gateway.id
   parent_id   = aws_api_gateway_resource.product_resource.id
