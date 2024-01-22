a571090a8279618ed05904a20125e0020b7c49e0
wagnerlm <wagnerlink@gmail.com>
2024-01-22 20:15:07 -0300

    feature: add get order by od

--- main.tf
+++ main.tf
@@ -217,6 +217,30 @@ resource "aws_api_gateway_integration" "get_orders_integration" {
   content_handling        = "CONVERT_TO_TEXT"
 }
 
+resource "aws_api_gateway_resource" "order_by_id_resource" {
+  rest_api_id = aws_api_gateway_rest_api.mikes_api_gateway.id
+  parent_id   = aws_api_gateway_resource.order_resource.id
+  path_part   = "{orderId}"
+}
+
+resource "aws_api_gateway_method" "get_order_by_id" {
+  rest_api_id   = aws_api_gateway_rest_api.mikes_api_gateway.id
+  resource_id   = aws_api_gateway_resource.order_by_id_resource.id
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.cognito_authorizer.id
+}
+
+resource "aws_api_gateway_integration" "get_order_by_id_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.mikes_api_gateway.id
+  resource_id             = aws_api_gateway_resource.order_by_id_resource.id
+  http_method             = aws_api_gateway_method.get_order_by_id.http_method
+  integration_http_method = "GET"
+  type                    = "HTTP_PROXY"
+  uri                     = "http://mikes-ecs-alb-1631856801.us-east-2.elb.amazonaws.com:8080/orders/{orderId}"
+  content_handling        = "CONVERT_TO_TEXT"
+}
+
 # -- ------------------------------------------------------
 # -- route products
 # -- ------------------------------------------------------
